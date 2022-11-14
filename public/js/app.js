20d98bf73bac50c17e34713ad321868dc6598488
Rakib0101 <rakib.01jan@gmail.com>
2022-11-14 14:22:52 +0600

    feat: header complete for desktop version

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+//dropdown on  click //
+$(".custom-dropdown").on('click', function() {
+    $(".dropdown-content ul").slideToggle();
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,14 @@ module.exports = {
             },
         },
         colors: {
+          white: "#FFFFFF",
+          black: "#000000",
+          secondary: "#272343",
+          "grayscales-500": "#F0F2F3",
+          "grayscales-700": "#E1E3E6",
+          "grayscales-900": "#636270",
+          "dark-accents" : "#007580",
+          
         },
   },
   plugins: [],
