796710385eba92cca692534e5d5d91596c9219b2
Rakib0101 <rakib.01jan@gmail.com>
2022-11-14 15:32:17 +0600

    feat: banner slider complete for desktop version

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
+var swiper = new Swiper(".bannerSwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 //dropdown on  click //
 $(".custom-dropdown").on('click', function() {
     $(".dropdown-content ul").slideToggle();
