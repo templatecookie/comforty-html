e5d397a1750cdb2e83bd7a0ece685a2d05a12937
Rakib0101 <rakib.01jan@gmail.com>
2022-11-17 12:24:23 +0600

    feat: mobile menu added done with responsive

--- public/js/app.js
+++ public/js/app.js
@@ -235,3 +235,32 @@ overlay.addEventListener('click', () => {
     overlay.classList.remove('open')
 })
 
+
+// tab js
+const tabs = document.querySelectorAll(".tabs");
+const tab = document.querySelectorAll(".tab");
+const panel = document.querySelectorAll(".tab-content");
+
+function onTabClick(event) {
+
+    // deactivate existing active tabs and panel
+
+    for (let i = 0; i < tab.length; i++) {
+        tab[i].classList.remove("active");
+    }
+
+    for (let i = 0; i < panel.length; i++) {
+        panel[i].classList.remove("active");
+    }
+
+
+    // activate new tabs and panel
+    event.target.classList.add('active');
+    let classString = event.target.getAttribute('data-target');
+    console.log(classString);
+    document.getElementById('panels').getElementsByClassName(classString)[0].classList.add("active");
+}
+
+for (let i = 0; i < tab.length; i++) {
+    tab[i].addEventListener('click', onTabClick, false);
+}
