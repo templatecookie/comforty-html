066181eca9a665ed3bafca7c810b3ba73b4f7583
Rakib0101 <rakib.01jan@gmail.com>
2022-11-18 19:45:43 +0600

    feat: header and footer added in all pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,4 @@
 //password field show and hide
-// $(".toggle-password").click(function() {
-
-//     $(this).toggleClass("svg-icon");
-//     var input = $($(this).attr("toggle"));
-//     if (input.attr("type") == "password") {
-//       input.attr("type", "text");
-//     } else {
-//       input.attr("type", "password");
-//     }
-//   });
-
-
-//   const togglePassword = document.querySelector('#togglePassword');
-//   const password = document.querySelector('#id_password');
-
-//   togglePassword.addEventListener('click', function (e) {
-//     // toggle the type attribute
-//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     // toggle the eye slash icon
-//     this.classList.toggle('svg-icon');
-// });
-
-//password field show and hide
 
 function PasswordIcon(){
     var x = document.getElementById("myInput");
@@ -57,23 +33,6 @@ function CreatePasswordIcon(){
 }
 
 
-//   const togglePassword = document.querySelector("#togglePassword");
-//         const password = document.querySelector("#password");
-
-//         togglePassword.addEventListener("click", function () {
-//             // toggle the type attribute
-//             const type = password.getAttribute("type") === "password" ? "text" : "password";
-//             password.setAttribute("type", type);
-            
-//             // toggle the icon
-//             this.classList.toggle("bi-eye");
-//         });
-
-//         // prevent form submit
-//         const form = document.querySelector("form");
-//         form.addEventListener('submit', function (e) {
-//             e.preventDefault();
-//         });
 var swiper = new Swiper(".bannerSwiper", {
     cssMode: true,
     loop: true,
